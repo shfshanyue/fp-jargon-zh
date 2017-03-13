c3ceb5b837934abc2cfa02a306721ad118f38d15
shfshanyue <xianger94@gmail.com>
2017-03-13 13:53:15 +0800

    map: derivated from ap

--- demos/monad.js
+++ demos/monad.js
@@ -2,6 +2,10 @@ Array.prototype.chain = function (f) {
   return this.reduce((acc, it) => acc.concat(f(it)), [])  
 }
 
+Array.prototype.ap = function (xs) {
+  return this.reduce((acc, f) => acc.concat(xs.map(f)), [])
+}
+
 // ['cat', 'dog', 'fish', 'bird']
 const a = Array.of('cat,dog', 'fish,bird').chain(s => s.split(','))
 
@@ -13,7 +17,8 @@ const b = animals.map(f)
 const c = animals.chain(x => {
   return Array.of(f(x))
 })
+const d = [f].ap(animals)
 
 // map :: function(f) { return this.chain(a => this.of(f(a))); }
-console.log(a, b, c)
+console.log(a, b, c, d)
 
